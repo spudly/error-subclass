e112de309e386fba5ab732519ad67f203a5f4d69
Stephen Sorensen <stephen.sorensen@theice.com>
2015-06-24 19:23:42 -0400

    initial commit

---
+++ ErrorSubclass.js
@@ -0,0 +1,34 @@
+class ErrorSubclass extends Error {
+
+  constructor (message) {
+    super(message);
+
+    if (this.constructor === ErrorSubclass) {
+      throw new Error('Don\'t instantiate ErrorSubclass directly. Extend it.');
+    }
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+    else {
+      Object.defineProperty(this, 'stack', {
+        value: (new Error()).stack
+      });
+    }
+
+    Object.defineProperty(this, 'message', {
+      value: message
+    });
+  }
+
+  get name() {
+    return this.constructor.name;
+  }
+
+  toString() {
+    return this.name + ': ' + this.message;
+  }
+
+}
+
+export default ErrorSubclass;
---
+++ test/ErrorSubclassTest.js
@@ -0,0 +1,54 @@
+import ErrorSubclass from '../ErrorSubclass';
+import {ok, equal, throws, doesNotThrow} from 'assert';
+
+const ERROR_MESSAGE = '__MESSAGE__'
+
+describe('ErrorSubclass', function() {
+
+  it('can\'t be directly instantiated', function() {
+    throws(function() {
+      new ErrorSubclass(ERROR_MESSAGE);
+    });
+  });
+
+  describe('Subclass of ErrorSubclass', function() {
+
+    class SubSubError extends ErrorSubclass {}
+
+    it('can be instantiated', function() {
+      doesNotThrow(function() {
+        new SubSubError(ERROR_MESSAGE);
+      });
+    });
+
+    describe('SubSubclass instance', function() {
+      var instance;
+
+      before(function() {
+        instance = new SubSubError(ERROR_MESSAGE);
+      });
+
+      it('should have a name', function() {
+        equal(instance.name, 'SubSubError');
+      });
+
+      it('should have a stack', function() {
+        ok(instance.stack);
+        equal(typeof instance.stack, 'string');
+        ok(instance.stack.match('SubSubError: ' + ERROR_MESSAGE));
+      });
+
+      it('should have a message', function() {
+        equal(instance.message, ERROR_MESSAGE);
+      });
+
+      it('has a toString() method', function() {
+        equal(typeof instance.toString, 'function');
+        equal(instance.toString(), 'SubSubError: ' + ERROR_MESSAGE);
+      });
+
+    });
+
+  });
+
+});
