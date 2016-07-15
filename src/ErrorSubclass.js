f6c93c0ebb4e7fa01a997d6e100587888353cc2c
spudly <shuoink@gmail.com>
2016-07-14 20:01:50 -0400

    chore(eslint) use eslint instead of jshint/standard

--- src/ErrorSubclass.js
+++ src/ErrorSubclass.js
@@ -1,30 +1,27 @@
-function ErrorSubclass (message) {
+function ErrorSubclass(message) {
   Error.call(this, message);
-  if (this.constructor === ErrorSubclass) {
-    throw new Error('Don\'t instantiate ErrorSubclass directly. Extend it.');
-  }
 
   if (Error.captureStackTrace) {
     Error.captureStackTrace(this, this.constructor);
   } else {
     Object.defineProperty(this, 'stack', {
-      value: (new Error()).stack
+      value: (new Error()).stack,
     });
   }
 
-  Object.defineProperty(this, 'message', {value: message});
+  Object.defineProperty(this, 'message', { value: message });
 }
 
 ErrorSubclass.prototype = Object.create(Error.prototype, {
   name: {
-    get: function () {
+    get() {
       return this.constructor.name;
-    }
-  }
+    },
+  },
 });
 
-ErrorSubclass.prototype.toString = function () {
-  return this.name + ': ' + this.message;
+ErrorSubclass.prototype.toString = function toString() {
+  return `${this.name}: ${this.message}`;
 };
 
 export default ErrorSubclass;
--- test/ErrorSubclassTest.js
+++ test/ErrorSubclassTest.js
@@ -1,59 +1,64 @@
 import ErrorSubclass from '../src/ErrorSubclass';
-import {ok, equal, throws, doesNotThrow} from 'assert';
+import { ok, equal, doesNotThrow } from 'assert';
 
 const ERROR_MESSAGE = '__MESSAGE__';
 
 class SubSubError extends ErrorSubclass {}
 
-var instance;
+const captureStackTrace = Error.captureStackTrace;
 
-var tests = {
+const tests = {
 
   ErrorSubclass: {
 
     'Subclass of ErrorSubclass': {
 
-      'can be instantiated': function () {
-        doesNotThrow(function () {
-          instance = new SubSubError(ERROR_MESSAGE);
-        });
+      'can be instantiated': () => {
+        doesNotThrow(() => new SubSubError(ERROR_MESSAGE));
       },
 
       'SubSubclass instance': {
 
-        before: function () {
-          instance = new SubSubError(ERROR_MESSAGE);
+        afterEach() {
+          Error.captureStackTrace = captureStackTrace;
         },
 
-        'should be instanceof Error': function () {
+        'should be instanceof Error': () => {
+          const instance = new SubSubError(ERROR_MESSAGE);
           ok(instance instanceof Error);
           ok(instance instanceof SubSubError);
         },
 
-        'should have a name': function () {
+        'should have a name': () => {
+          const instance = new SubSubError(ERROR_MESSAGE);
           equal(instance.name, 'SubSubError');
         },
 
-        'should have a stack': function () {
-          ok(instance.stack);
-          equal(typeof instance.stack, 'string');
-          ok(instance.stack.match('SubSubError: ' + ERROR_MESSAGE));
-        },
-
-        'should have a message': function () {
+        'should have a message': () => {
+          const instance = new SubSubError(ERROR_MESSAGE);
           equal(instance.message, ERROR_MESSAGE);
         },
 
-        'has a toString() method': function () {
+        'has a toString() method': () => {
+          const instance = new SubSubError(ERROR_MESSAGE);
           equal(typeof instance.toString, 'function');
-          equal(instance.toString(), 'SubSubError: ' + ERROR_MESSAGE);
-        }
+          equal(instance.toString(), `SubSubError: ${ERROR_MESSAGE}`);
+        },
 
-      }
+        'when captureStackTrace is supported, should have a stack': () => {
+          equal(typeof Error.captureStackTrace, 'function');
+          const instance = new SubSubError(ERROR_MESSAGE);
+          ok(instance.stack);
+          equal(typeof instance.stack, 'string');
+          ok(instance.stack.match(`SubSubError: ${ERROR_MESSAGE}`));
+        },
 
-    }
-
-  }
+        // 'when captureStackTrace is not supported, should have a stack': () => {
+        //   // TODO: how to test?
+        // },
+      },
+    },
+  },
 };
 
 export default tests;
