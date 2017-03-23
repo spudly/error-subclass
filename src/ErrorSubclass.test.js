e949360e641d0295faf754e17eb7507300f1f44b
Stephen Sorensen <shuoink@gmail.com>
2017-03-22 21:39:34 -0400

    chore(tests): switch to jest

--- src/ErrorSubclass.test.js
+++ src/ErrorSubclass.test.js
@@ -1,4 +1,3 @@
-import {doesNotThrow, equal, ok} from 'assert';
 import ErrorSubclass from '../src/ErrorSubclass';
 
 const ERROR_MESSAGE = '__MESSAGE__';
@@ -9,54 +8,46 @@ class SubSubError extends ErrorSubclass {
 
 const {captureStackTrace} = Error;
 
-const tests = {
-  ErrorSubclass: {
-    'Subclass of ErrorSubclass': {
-      'can be instantiated': () => {
-        doesNotThrow(() => new SubSubError(ERROR_MESSAGE));
-      },
+afterEach(() => {
+  Error.captureStackTrace = captureStackTrace;
+});
 
-      'SubSubclass instance': {
-        afterEach() {
-          Error.captureStackTrace = captureStackTrace;
-        },
+test('Subclass of ErrorSubclass can be instantiated', () => {
+  expect(() => new SubSubError(ERROR_MESSAGE)).not.toThrow();
+});
 
-        'should be instanceof Error': () => {
-          const instance = new SubSubError(ERROR_MESSAGE);
-          ok(instance instanceof Error);
-          ok(instance instanceof SubSubError);
-        },
+test('SubSubclass instance should be instanceof Error', () => {
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(instance).toBeInstanceOf(Error);
+  expect(instance).toBeInstanceOf(SubSubError);
+});
 
-        'should have a name that matches the displayName': () => {
-          const instance = new SubSubError(ERROR_MESSAGE);
-          equal(instance.name, 'SubSubErrorDisplayName');
-        },
+test('SubSubclass instance should have a name that matches the displayName', () => {
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(instance.name).toBe('SubSubErrorDisplayName');
+});
 
-        'should have a message': () => {
-          const instance = new SubSubError(ERROR_MESSAGE);
-          equal(instance.message, ERROR_MESSAGE);
-        },
+test('SubSubclass instance should have a message', () => {
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(instance.message).toBe(ERROR_MESSAGE);
+});
 
-        'has a toString() method': () => {
-          const instance = new SubSubError(ERROR_MESSAGE);
-          equal(typeof instance.toString, 'function');
-          equal(instance.toString(), `SubSubErrorDisplayName: ${ERROR_MESSAGE}`);
-        },
+test('SubSubclass instance has a toString() method', () => {
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(typeof instance.toString).toBe('function');
+  expect(instance.toString()).toBe(`SubSubErrorDisplayName: ${ERROR_MESSAGE}`);
+});
 
-        'when captureStackTrace is supported, should have a stack': () => {
-          equal(typeof Error.captureStackTrace, 'function');
-          const instance = new SubSubError(ERROR_MESSAGE);
-          ok(instance.stack);
-          equal(typeof instance.stack, 'string');
-          ok(instance.stack.match(`SubSubErrorDisplayName: ${ERROR_MESSAGE}`));
-        }
+test('SubSubclass instance when captureStackTrace is supported, should have a stack', () => {
+  expect(typeof Error.captureStackTrace).toBe('function');
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(typeof instance.stack).toBe('string');
+  expect(instance.stack).toMatch(`SubSubErrorDisplayName: ${ERROR_MESSAGE}`);
+});
 
-        // 'when captureStackTrace is not supported, should have a stack': () => {
-        //   // TODO: how to test?
-        // },
-      }
-    }
-  }
-};
-
-export default tests;
+test('SubSubclass, when captureStackTrace is not supported, should still have a stack', () => {
+  Reflect.deleteProperty(Error, 'captureStackTrace');
+  expect(Error.captureStackTrace).not.toBeDefined();
+  const instance = new SubSubError(ERROR_MESSAGE);
+  expect(typeof instance.stack).toBe('string');
+});
